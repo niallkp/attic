87fa74ce60a1e32f018566c986c61039cf6f0ccd
Sebb <sebb@apache.org>
2025-01-20 22:53:58 +0000

    Set up cwiki, svn and git entries
    git-svn-id: https://svn.apache.org/repos/asf/attic/site@1923292 13f79535-47bb-0310-9956-ffa450edef68

--- retire.py
+++ retire.py
@@ -28,6 +28,7 @@ from inspect import getsourcefile
 from string import Template
 import os
 import re
+import subprocess
 from collections import defaultdict
 from urlutils import loadyaml, loadjson
 
@@ -43,6 +44,19 @@ for entry in loadjson(CWIKI_INFO)['results']:
     WIKI_KEYS[entry['name'].lower().replace('apache ', '')].append(entry['key'])
 
 JIRA='https://issues.apache.org/jira/rest/api/2/project'
+
+SVNURL='https://svn.apache.org/repos/asf/'
+SVNKEYS = {}
+svnargs = ['svn', 'ls', SVNURL]
+output = subprocess.run(svnargs, stdout=subprocess.PIPE, check=True).stdout.decode("us-ascii")
+for name in output.split('\n'):
+    if len(name) > 0:
+        SVNKEYS[name.rstrip('/')] = ''
+
+GITREPOS='https://gitbox.apache.org/repositories.json'
+GITKEYS = {}
+for entry in loadjson(GITREPOS)['projects']:
+    GITKEYS[entry] = ''
 
 MYHOME = dirname(abspath(getsourcefile(lambda:0)))
 PROJECTS =    join((MYHOME), 'xdocs', 'projects')
@@ -123,11 +137,30 @@ def create_project(pid):
     mnames = lists[pid]
     mnames.remove('dev')
     jiras = list_jira(pid)
+    # Is there a Wiki?
+    cwikis = find_wiki(pid)
+    if len(cwikis) > 0:
+        if len(cwikis) == 1 and cwikis[0].lower() == pid:
+            cwiki ='<cwiki/>'
+        else:
+            keys = ','.join(sorted(cwikis))
+            cwiki =f'<cwiki keys="{keys}"/>'
+    else:
+        cwiki = ''
+    svn = git = ''
+    # May have both SVN and Git
+    if pid in SVNKEYS:
+        svn = '<svn/>'
+    if pid in GITKEYS:
+        git = '<git/>'
     out = template.substitute(tlpid = pid,
         FullName = meta['display_name'],
         Month_Year = meta['retired'],
         mail_names = ",".join(sorted(mnames)),
         jira_names = ",".join(sorted(jiras)),
+        cwiki = cwiki,
+        svn = svn,
+        git = git,
         description = meta.get('description', 'TBA'))
     with open(outfile, 'w', encoding='utf-8') as o:
         o.write(out)
